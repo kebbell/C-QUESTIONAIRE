3aa848f1ede730963ad8d8eb7869f4acc841b5f3
Sam <samuelkebbell@gmail.com>
2024-11-17 14:23:02 +0000

    Updates

--- Program.cs
+++ Program.cs
@@ -160,3 +160,12 @@ Console.WriteLine("Result of specified numbers " + num18 + ", " + num19 + " and 
 int num21 = 25;
 
 Console.WriteLine("You look older than " + num21);
+
+// 12. Write a C# program that takes a number as input and displays it four times in a row (separated by blank spaces), and then four times in the next row, with no separation. You should do it twice: Use the console. Write and use {0}.
+// Test Data:
+// Enter a digit: 25
+// Expected Output:
+// 25 25 25 25
+// 25252525
+// 25 25 25 25
+// 25252525
