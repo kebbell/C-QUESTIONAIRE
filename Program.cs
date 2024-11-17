a397b4fe1f1e389efd7df2e54abdc4b258654f26
Sam <samuelkebbell@gmail.com>
2024-11-17 14:25:34 +0000

    Updates

--- Program.cs
+++ Program.cs
@@ -176,3 +176,22 @@ Console.WriteLine(num22 + " " + num22 + " " + num22 + " " + num22);
 Console.WriteLine(num22 + num22 + num22 + num22 + num22 + num22 + num22 + num22);
 Console.WriteLine(num22 + " " + num22 + " " + num22 + " " + num22);
 Console.WriteLine(num22 + num22 + num22 + num22 + num22 + num22 + num22 + num22);
+
+// 13. Write a C# program that takes a number as input and displays a rectangle of 3 columns wide and 5 rows tall using that digit.
+// Test Data:
+// Enter a number: 5
+// Expected Output:
+// 555
+// 5 5
+// 5 5
+// 5 5
+// 555
+
+int num23 = 5;
+
+Console.WriteLine(num23 + num23 + num23);
+Console.WriteLine(num23 + " " + num23);
+Console.WriteLine(num23 + " " + num23);
+Console.WriteLine(num23 + " " + num23);
+Console.WriteLine(num23 + num23 + num23);
+
--- obj/Debug/net9.0/app.AssemblyInfo.cs
+++ obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa848f1ede730963ad8d8eb7869f4acc841b5f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea1d44eca965223353739ad907331653f3f99f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
