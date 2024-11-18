24e87efe3375e3eb6782516da4c89d9776d754cb
Sam <samuelkebbell@gmail.com>
2024-11-18 16:54:15 +0000

    Updates

---
+++ Test2.cs
@@ -0,0 +1,193 @@
+// 21. Write a C# program to check the sum of the two given integers. Return true if one of the integers is 20 or if their sum is 20.
+// Click me to see the solution
+
+
+
+
+// 22. Write a C# program to check if the given integer is within 20 of 100 or 200.
+// Sample Output:
+// Input an integer:
+// 25
+// False
+// Click me to see the solution
+
+
+
+
+// 23. Write a C# program to convert a given string into lowercase.
+// Sample Output:
+// write a c# sharp program to display the following pattern using the alphabet.
+// Click me to see the solution
+
+
+
+
+// 24. Write a C# program to find the longest word in a string.
+// Test Data: Write a C# Sharp Program to display the following pattern using the alphabet.
+// Sample Output:
+// following
+// Click me to see the solution
+
+
+
+// 25. Write a C# program to print odd numbers from 1 to 99. Prints one number per line.
+// Sample Output:
+// Odd numbers from 1 to 99. Prints one number per line.
+// 1
+// 3
+// 5
+// 7
+// 9
+// ...
+// 95
+// 97
+// 99
+// Click me to see the solution
+
+
+
+// 26. Write a C# program to compute the sum of the first 500 prime numbers.
+// Sample Output:
+// Sum of the first 500 prime numbers:
+// 824693
+// Click me to see the solution
+
+
+
+// 27. Write a C# program and compute the sum of an integer's digits.
+// Sample Output:
+// Input a number(integer): 12
+// Sum of the digits of the said integer: 3
+// Click me to see the solution
+
+
+
+// 28. Write a C# program to reverse the words of a sentence.
+// Sample Output:
+// Original String: Display the pattern like pyramid using the alphabet.
+// Reverse String: alphabet. the using pyramid like pattern the Display
+// Click me to see the solution
+
+
+
+// 29. Write a C# program to find the size of a specified file in bytes.
+// Sample Output:
+// Size of a file: 31
+// Click me to see the solution
+
+
+
+// 30. Write a C# program to convert a hexadecimal number to a decimal number.
+// Sample Output:
+// Hexadecimal number: 4B0
+// Convert to-
+// Decimal number: 1200
+// Click me to see the solution
+
+
+
+// 31. Write a C# program to multiply the corresponding elements of two integer arrays.
+// Sample Output:
+// Array1: [1, 3, -5, 4]
+// Array2: [1, 4, -5, -2]
+// Multiply corresponding elements of two arrays:
+// 1 12 25 -8
+// Click me to see the solution
+
+
+
+// 32. Write a C# program to create a string of four copies, taking the last four characters from a given string. If the given string is less than 4, return the original one.
+// Sample Output:
+// Input a string : The quick brown fox jumps over the lazy dog.
+// dog.dog.dog.dog.
+// Click me to see the solution
+
+
+
+// 33. Write a C# program to check if a given positive number is a multiple of 3 or 7.
+// Sample Output:
+// Input first integer:
+// 15
+// True
+// Click me to see the solution
+
+
+
+// 34. Write a C# program to check if a string starts with a specified word.
+// Note: Suppose the sentence starts with "Hello"
+// Sample Data: string1 = "Hello how are you?"
+// Result: Hello.
+// Sample Output:
+// Input a string : Hello how are you?
+// True
+// Click me to see the solution
+
+
+
+// 35. Write a C# program to check two given numbers where one is less than 100 and the other is greater than 200.
+// Sample Output:
+// Input a first number(<100): 75
+// Input a second number(>100): 250
+// True
+// Click me to see the solution
+
+
+
+// 36. Write a C# program to check if an integer (from the two given integers) is in the range -10 to 10.
+// Sample Output:
+// Input a first number: -5
+// Input a second number: 8
+// True
+// Click me to see the solution
+
+
+
+// 37. Write a C# program to check if "HP" appears at the second position in a string and return the string without "HP".
+// Test Data: PHP Tutorial
+// Sample Output:
+// P Tutorial
+// Click me to see the solution
+
+
+
+// 38. Write a C# program to get a string of two characters from a given string. The first and second characters of the given string must be "P" and "H", so PHP will be "PH".
+// Test Data: PHP
+// Sample Output:
+// PH
+// Click me to see the solution
+
+
+
+// 39. Write a C# program to find the largest and lowest values from three integer values.
+// Test Data:
+// Input first integer:
+// 15
+// Input second integer:
+// 25
+// Input third integer:
+// 30
+// Sample Output
+// Largest of three: 30
+// Lowest of three: 15
+// Click me to see the solution
+
+
+
+// 40. Write a C# program that checks the nearest value of 20 of two given integers and return 0 if two numbers are same.
+// Test Data:
+// Input first integer:
+// 15
+// Input second integer:
+// 12
+// Sample Output
+// 15
+// Click me to see the solution
+
+
+
+// 41. Write a C# program to check if a given string contains the 'w' character between 1 and 3 times.
+// Test Data:
+// Input a string (contains at least one 'w' char) : w3resource
+// Test the string contains 'w' character between 1 and 3 times:
+// Sample Output
+// True
