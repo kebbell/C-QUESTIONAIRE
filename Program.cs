134e1d75d6ec54e57c9993838f996897de341ca6
Sam <samuelkebbell@gmail.com>
2024-11-18 13:40:30 +0000

    ru

--- Program.cs
+++ Program.cs
@@ -263,6 +263,10 @@ Console.WriteLine((num25 < 0 && num26 > 0) || (num25 > 0 && num26 < 0));
 
 // 19. Write a C# program to compute the sum of two given integers. If two values are the same, return the triple of their sum.
 
+int num27 = 2;
+int num28 = 2;
+
+Console.WriteLine((num27 == num28) ? (num27 + num28) * 3 : num27 + num28);
 
 
 // 20. Write a C# program to get the absolute value of the difference between two given numbers. Return double the absolute value of the difference if the first number is greater than the second number.
