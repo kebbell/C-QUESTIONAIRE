1b25ebbc837dc548423e9ffbd5a71157fb6390ba
Sam <samuelkebbell@gmail.com>
2024-12-15 15:37:45 +0000

    Updates

--- Test2/Test8.cs
+++ Test2/Test8.cs
@@ -1,238 +1,238 @@
-// 41. Write a C# Sharp program that accepts two integers and returns true if either is 5 or their sum or difference is 5.
-// Sample Input:
-// 5, 4
-// 4, 3
-// 1, 4
-// Expected Output:
-// True
-// False
-// True
-// Click me to see the solution
+// // 41. Write a C# Sharp program that accepts two integers and returns true if either is 5 or their sum or difference is 5.
+// // Sample Input:
+// // 5, 4
+// // 4, 3
+// // 1, 4
+// // Expected Output:
+// // True
+// // False
+// // True
+// // Click me to see the solution
 
-int a = 5, b = 4;
-if (a == 5 || b == 5 || a + b == 5 || a - b == 5)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
-
-
-// 42. Write a C# Sharp program to test if a given non-negative number is a multiple of 13 or one more than a multiple of 13.
-// Sample Input:
-// 13
-// 14
-// 27
-// 41
-// Expected Output:
-// True
-// True
-// True
-// False
-// Click me to see the solution
-
-int c = 13;
-if (c % 13 == 0 || c % 13 == 1)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+// int a = 5, b = 4;
+// if (a == 5 || b == 5 || a + b == 5 || a - b == 5)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
 
 
-// 43. Write a C# Sharp program to check if a given number that is not negative is a multiple of 3 or 7, but not both.
-// Sample Input:
-// 3
-// 7
-// 21
-// Expected Output:
-// True
-// True
-// False
-// Click me to see the solution
+// // 42. Write a C# Sharp program to test if a given non-negative number is a multiple of 13 or one more than a multiple of 13.
+// // Sample Input:
+// // 13
+// // 14
+// // 27
+// // 41
+// // Expected Output:
+// // True
+// // True
+// // True
+// // False
+// // Click me to see the solution
 
-int d = 3;
-if (d % 3 == 0 && d % 7 != 0)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
-
-// 44. Write a C# Sharp program to check if a given number is within 2 of a multiple of 10.
-// Sample Input:
-// 3
-// 7
-// 8
-// 21
-// Expected Output:
-// False
-// False
-// True
-// True
-// Click me to see the solution
-
-int e = 3;
-if (e % 10 == 2 || e % 10 == -2)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
-
-// 45. Write a C# Sharp program to compute the sum of the two given integers. Return 18 if one of the given integer values is in the range of 10..20 inclusive.
-// Sample Input:
-// 3, 7
-// 10, 11
-// 10, 20
-// 21, 220
-// Expected Output:
-// 10
-// 18
-// 18
-// 241
-// Click me to see the solution
-
-int f = 3, g = 7;
-if (f >= 10 && f <= 20 || g >= 10 && g <= 20)
-{
-    Console.WriteLine("18");
-}
-else
-{
-    Console.WriteLine(f + g);
-}
-
-// 46. Write a C# Sharp program to check whether a given string begins with "F" or ends with "B". If the string starts with "F" return "Fizz" and return "Buzz" if it ends with "B" If the string starts with "F" and ends with "B" return "FizzBuzz". In other cases return the original string.
-// In other cases return the original string.
-// Sample Input:
-// "FizzBuzz"
-// "Fizz"
-// "Buzz"
-// "Founder"
-// Expected Output:
-// Fizz
-// Fizz
-// Buzz
-// Fizz
-// Click me to see the solution
-
-string h = "FizzBuzz";
-if (h.StartsWith("F") && h.EndsWith("B"))
-{
-    Console.WriteLine("FizzBuzz");
-}
-else if (h.StartsWith("F"))
-{
-    Console.WriteLine("Fizz");
-}
-else if (h.EndsWith("B"))
-{
-    Console.WriteLine("Buzz");
-}
-else
-{
-    Console.WriteLine(h);
-}
-
-// 47. Write a C# Sharp program to check if it is possible to add two integers to get the third integer from three given integers.
-// Sample Input:
-// 1, 2, 3
-// 4, 5, 6
-// -1, 1, 0
-// Expected Output:
-// True
-// False
-// True
-// Click me to see the solution
-
-int i = 1, j = 2, k = 3;
-if (i + j == k || i + k == j || j + k == i)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
-
-// 48. Write a C# Sharp program to check if y is greater than x, and z is greater than y from three given integers x,y,z.
-// Sample Input:
-// 1, 2, 3
-// 4, 5, 6
-// -1, 1, 0
-// Expected Output:
-// True
-// True
-// False
-// Click me to see the solution
-
-int l = 1, m = 2, n = 3;
-if (m > l && n > m)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+// int c = 13;
+// if (c % 13 == 0 || c % 13 == 1)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
 
 
-// 49. Write a C# Sharp program to check if three given numbers are in strict increasing order.
-// For example, 4 7 15, or 45, 56, 67, but not 4 ,5, 8 or 6, 6, 8. However, if a fourth parameter is true, equality is allowed, such as 6, 6, 8 or 7, 7, 7.
-// Sample Input:
-// 1, 2, 3, false
-// 1, 2, 3, true
-// 10, 2, 30, false
-// 10, 10, 30, true
-// Expected Output:
-// True
-// True
-// False
-// True
-// Click me to see the solution
+// // 43. Write a C# Sharp program to check if a given number that is not negative is a multiple of 3 or 7, but not both.
+// // Sample Input:
+// // 3
+// // 7
+// // 21
+// // Expected Output:
+// // True
+// // True
+// // False
+// // Click me to see the solution
 
-int o = 1, p = 2, q = 3;    
-if (o < p && p < q || o == p && p == q)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+// int d = 3;
+// if (d % 3 == 0 && d % 7 != 0)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
+
+// // 44. Write a C# Sharp program to check if a given number is within 2 of a multiple of 10.
+// // Sample Input:
+// // 3
+// // 7
+// // 8
+// // 21
+// // Expected Output:
+// // False
+// // False
+// // True
+// // True
+// // Click me to see the solution
+
+// int e = 3;
+// if (e % 10 == 2 || e % 10 == -2)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
+
+// // 45. Write a C# Sharp program to compute the sum of the two given integers. Return 18 if one of the given integer values is in the range of 10..20 inclusive.
+// // Sample Input:
+// // 3, 7
+// // 10, 11
+// // 10, 20
+// // 21, 220
+// // Expected Output:
+// // 10
+// // 18
+// // 18
+// // 241
+// // Click me to see the solution
+
+// int f = 3, g = 7;
+// if (f >= 10 && f <= 20 || g >= 10 && g <= 20)
+// {
+//     Console.WriteLine("18");
+// }
+// else
+// {
+//     Console.WriteLine(f + g);
+// }
+
+// // 46. Write a C# Sharp program to check whether a given string begins with "F" or ends with "B". If the string starts with "F" return "Fizz" and return "Buzz" if it ends with "B" If the string starts with "F" and ends with "B" return "FizzBuzz". In other cases return the original string.
+// // In other cases return the original string.
+// // Sample Input:
+// // "FizzBuzz"
+// // "Fizz"
+// // "Buzz"
+// // "Founder"
+// // Expected Output:
+// // Fizz
+// // Fizz
+// // Buzz
+// // Fizz
+// // Click me to see the solution
+
+// string h = "FizzBuzz";
+// if (h.StartsWith("F") && h.EndsWith("B"))
+// {
+//     Console.WriteLine("FizzBuzz");
+// }
+// else if (h.StartsWith("F"))
+// {
+//     Console.WriteLine("Fizz");
+// }
+// else if (h.EndsWith("B"))
+// {
+//     Console.WriteLine("Buzz");
+// }
+// else
+// {
+//     Console.WriteLine(h);
+// }
+
+// // 47. Write a C# Sharp program to check if it is possible to add two integers to get the third integer from three given integers.
+// // Sample Input:
+// // 1, 2, 3
+// // 4, 5, 6
+// // -1, 1, 0
+// // Expected Output:
+// // True
+// // False
+// // True
+// // Click me to see the solution
+
+// int i = 1, j = 2, k = 3;
+// if (i + j == k || i + k == j || j + k == i)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
+
+// // 48. Write a C# Sharp program to check if y is greater than x, and z is greater than y from three given integers x,y,z.
+// // Sample Input:
+// // 1, 2, 3
+// // 4, 5, 6
+// // -1, 1, 0
+// // Expected Output:
+// // True
+// // True
+// // False
+// // Click me to see the solution
+
+// int l = 1, m = 2, n = 3;
+// if (m > l && n > m)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
+
+
+// // 49. Write a C# Sharp program to check if three given numbers are in strict increasing order.
+// // For example, 4 7 15, or 45, 56, 67, but not 4 ,5, 8 or 6, 6, 8. However, if a fourth parameter is true, equality is allowed, such as 6, 6, 8 or 7, 7, 7.
+// // Sample Input:
+// // 1, 2, 3, false
+// // 1, 2, 3, true
+// // 10, 2, 30, false
+// // 10, 10, 30, true
+// // Expected Output:
+// // True
+// // True
+// // False
+// // True
+// // Click me to see the solution
+
+// int o = 1, p = 2, q = 3;    
+// if (o < p && p < q || o == p && p == q)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
 
 
 
 
-// 50. Write a C# Sharp program to check if two or more integers that are not negative have the same rightmost digit.
-// Sample Input:
-// 11, 21, 31
-// 11, 22, 31
-// 11, 22, 33
-// Expected Output:
-// True
-// True
-// False
-// Click me to see the solution
+// // 50. Write a C# Sharp program to check if two or more integers that are not negative have the same rightmost digit.
+// // Sample Input:
+// // 11, 21, 31
+// // 11, 22, 31
+// // 11, 22, 33
+// // Expected Output:
+// // True
+// // True
+// // False
+// // Click me to see the solution
 
-int r = 11, s = 21, t = 31;
-if (r % 10 == s % 10 || r % 10 == t % 10 || s % 10 == t % 10)
-{
-    Console.WriteLine("True");
-}
-else
-{
-    Console.WriteLine("False");
-}
+// int r = 11, s = 21, t = 31;
+// if (r % 10 == s % 10 || r % 10 == t % 10 || s % 10 == t % 10)
+// {
+//     Console.WriteLine("True");
+// }
+// else
+// {
+//     Console.WriteLine("False");
+// }
 
 
 // 51. Write a C# Sharp program to check three given integers and return true if one of them is 20 or more less than one of the others.
