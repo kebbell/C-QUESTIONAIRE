a4b972127152a3f123005c2d0c4388a067af790e
Sam <samuelkebbell@gmail.com>
2024-12-17 13:29:35 +0000

    Updates

--- obj/Debug/net9.0/app.AssemblyInfo.cs
+++ obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13035e018d504c816e99b1ea8653051693d701d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c2f88d5734566761b8a28cd0c61f95f68a412f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
