2273491568d11e76a1727b2f59a6b229fdd20293
Sam <samuelkebbell@gmail.com>
2024-12-19 15:00:11 +0000

    Updates

--- Test2/Test9.cs
+++ Test2/Test9.cs
@@ -295,7 +295,6 @@ Console.WriteLine(s83);
 Console.WriteLine(s84);
 
 
-
 // 76. Write a C# Sharp program to create a new string of length 2, using the first two characters of a given string. If the given string length is less than 2 use '#' as missing characters.
 // Sample Input:
 // "Hello"
