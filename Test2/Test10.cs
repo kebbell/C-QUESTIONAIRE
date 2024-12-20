0d954c137ecb487fe1c91a9f12bae0afac3c0ac3
Sam <samuelkebbell@gmail.com>
2024-12-20 13:37:56 +0000

    Updates

--- Test2/Test10.cs
+++ Test2/Test10.cs
@@ -194,13 +194,11 @@
 
 
 // 97. Write a C# Sharp program to check if an array of integers length 2 contains 15 or 20.
-
 // Sample Input:
 // { 12, 20 }
 // { 14, 15 }
 // { 11, 21 }
 // Expected Output :
-
 // True
 // True
 // False
