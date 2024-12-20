e57e8b5e1e8b7b85adef33fa81ad2d625fa406c8
Sam <samuelkebbell@gmail.com>
2024-12-20 13:35:51 +0000

    Updates

--- Test2/Test10.cs
+++ Test2/Test10.cs
@@ -1,209 +1,209 @@
-81. Write a C# Sharp program to check whether the first two characters and the last two characters of a given string are the same.
+// 81. Write a C# Sharp program to check whether the first two characters and the last two characters of a given string are the same.
 
-Sample Input:
-"abab"
-"abcdef"
-"xyzsderxy"
-Expected Output:
+// Sample Input:
+// "abab"
+// "abcdef"
+// "xyzsderxy"
+// Expected Output:
 
-True
-False
-True
-Click me to see the solution
-82. Write a C# Sharp program to combine two given strings. If the given strings have different lengths remove the characters from the longer string.
+// True
+// False
+// True
+// Click me to see the solution
+// 82. Write a C# Sharp program to combine two given strings. If the given strings have different lengths remove the characters from the longer string.
 
-Sample Input:
-"abc", "abcd"
-"Python", "Python"
-"JS", "Python"
-Expected Output:
+// Sample Input:
+// "abc", "abcd"
+// "Python", "Python"
+// "JS", "Python"
+// Expected Output:
 
-abcbcd
-PythonPython
-JSon
-Click me to see the solution
-83. Write a C# Sharp program to create a new string using 3 copies of the first 2 characters of a given string. If the string length is less than 2 use the whole string.
+// abcbcd
+// PythonPython
+// JSon
+// Click me to see the solution
+// 83. Write a C# Sharp program to create a new string using 3 copies of the first 2 characters of a given string. If the string length is less than 2 use the whole string.
 
-Sample Input:
-"abc"
-"Python"
-"J"
-Expected Output:
+// Sample Input:
+// "abc"
+// "Python"
+// "J"
+// Expected Output:
 
-ababab
-PyPyPy
-JJJ
-Click me to see the solution
-84. Write a C# Sharp program to create a new string from a string. In the case that the two characters at the beginning and end of the given string are the same, return the given string without the first two characters, otherwise return the original string.
+// ababab
+// PyPyPy
+// JJJ
+// Click me to see the solution
+// 84. Write a C# Sharp program to create a new string from a string. In the case that the two characters at the beginning and end of the given string are the same, return the given string without the first two characters, otherwise return the original string.
 
-Sample Input:
-"abcab"
-"Python"
-Expected Output:
+// Sample Input:
+// "abcab"
+// "Python"
+// Expected Output:
 
-cab
-Python
-Click me to see the solution
-85. Write a C# Sharp program to create a new string from a given string without the first two characters. Keep the first character if it is "p" and keep the second character if it is "y".
+// cab
+// Python
+// Click me to see the solution
+// 85. Write a C# Sharp program to create a new string from a given string without the first two characters. Keep the first character if it is "p" and keep the second character if it is "y".
 
-Sample Input:
-"abcab"
-"python"
-"press"
-"jython"
-Expected Output:
+// Sample Input:
+// "abcab"
+// "python"
+// "press"
+// "jython"
+// Expected Output:
 
-cab
-python
-pess
-ython
-Click me to see the solution
-86. Write a C# Sharp program to create a string from a given string without the first and last character. This is if the first or last characters are 'a' otherwise return the original given string.
+// cab
+// python
+// pess
+// ython
+// Click me to see the solution
+// 86. Write a C# Sharp program to create a string from a given string without the first and last character. This is if the first or last characters are 'a' otherwise return the original given string.
 
-Sample Input:
-"abcab"
-"python"
-"abcda"
-"jython"
-Expected Output:
+// Sample Input:
+// "abcab"
+// "python"
+// "abcda"
+// "jython"
+// Expected Output:
 
-bcab
-python
-bcd
-jython
-Click me to see the solution
-87. Write a C# Sharp program to create a new string from a given string. Return the string without the first or first two 'a' characters otherwise return the original string.
+// bcab
+// python
+// bcd
+// jython
+// Click me to see the solution
+// 87. Write a C# Sharp program to create a new string from a given string. Return the string without the first or first two 'a' characters otherwise return the original string.
 
-Sample Input:
-"abcab"
-"python"
-"aacda"
-"jython"
-Expected Output:
+// Sample Input:
+// "abcab"
+// "python"
+// "aacda"
+// "jython"
+// Expected Output:
 
-bcab
-python
-cda
-jython
-Click me to see the solution
-88. Write a C# Sharp program to check a given array of integers of length 1 or more. Return true if 10 appears as either the first or last element in the given array.
+// bcab
+// python
+// cda
+// jython
+// Click me to see the solution
+// 88. Write a C# Sharp program to check a given array of integers of length 1 or more. Return true if 10 appears as either the first or last element in the given array.
 
-Sample Input:
-{ 10, 20, 40, 50 }
-{ 5, 20, 40, 10 }
-{ 10, 20, 40, 10 }
-{ 12, 24, 35, 55 }
-Expected Output:
+// Sample Input:
+// { 10, 20, 40, 50 }
+// { 5, 20, 40, 10 }
+// { 10, 20, 40, 10 }
+// { 12, 24, 35, 55 }
+// Expected Output:
 
-True
-True
-True
-False
-Click me to see the solution
-89. Write a C# Sharp program to check a given array of integers of length 1 or more. Return true if the first and the last element in the array are equal.
+// True
+// True
+// True
+// False
+// Click me to see the solution
+// 89. Write a C# Sharp program to check a given array of integers of length 1 or more. Return true if the first and the last element in the array are equal.
 
-Sample Input:
-{ 10, 20, 40, 50 }
-{ 10, 20, 40, 10 }
-{ 12, 24, 35, 55 }
-Expected Output:
+// Sample Input:
+// { 10, 20, 40, 50 }
+// { 10, 20, 40, 10 }
+// { 12, 24, 35, 55 }
+// Expected Output:
 
-False
-True
-False
-Click me to see the solution
-90. Write a C# Sharp program to check two given arrays of integers of length 1 or more. Return true if they have the same first element or the same last element.
+// False
+// True
+// False
+// Click me to see the solution
+// 90. Write a C# Sharp program to check two given arrays of integers of length 1 or more. Return true if they have the same first element or the same last element.
 
-Sample Input:
-{ 10, 20, 40, 50 }, { 10, 20, 40, 50 }
-{ 10, 20, 40, 50 }, { 10, 20, 40, 5 }
-{ 10, 20, 40, 50 }, { 1, 20, 40, 5 }
-Expected Output:
+// Sample Input:
+// { 10, 20, 40, 50 }, { 10, 20, 40, 50 }
+// { 10, 20, 40, 50 }, { 10, 20, 40, 5 }
+// { 10, 20, 40, 50 }, { 1, 20, 40, 5 }
+// Expected Output:
 
-True
-True
-False
-Click me to see the solution
-91. Write a C# Sharp program to compute the sum of the elements of an array of integers.
+// True
+// True
+// False
+// Click me to see the solution
+// 91. Write a C# Sharp program to compute the sum of the elements of an array of integers.
 
-Sample Input:
-{ 10, 20, 30, 40, 50 }
-{ 10, 20, -30, -40, 50 }
-Expected Output:
+// Sample Input:
+// { 10, 20, 30, 40, 50 }
+// { 10, 20, -30, -40, 50 }
+// Expected Output:
 
-150
-10
-Click me to see the solution
-92. Write a C# Sharp program to rotate the elements of a given array of integers (length 4 ) in the left direction and return the array.
+// 150
+// 10
+// Click me to see the solution
+// 92. Write a C# Sharp program to rotate the elements of a given array of integers (length 4 ) in the left direction and return the array.
 
-Sample Input:
-{ 10, 20, -30, -40 }
-Expected Output:
+// Sample Input:
+// { 10, 20, -30, -40 }
+// Expected Output:
 
-Rotated array: 20 -30 -40 10
-Click me to see the solution
+// Rotated array: 20 -30 -40 10
+// Click me to see the solution
 
-93. Write a C# Sharp program to reverse a given array of integers and length 5.
+// 93. Write a C# Sharp program to reverse a given array of integers and length 5.
 
-Sample Input:
-{ 10, 20, -30, -40, 50 }
-Expected Output:
+// Sample Input:
+// { 10, 20, -30, -40, 50 }
+// Expected Output:
 
-Reverse array: 50 -40 -30 20 10
-Click me to see the solution
-94. Write a C# Sharp program to find out the maximum element between the first and last element in a given array of integers ( length 4), replace all elements with the maximum element.
+// Reverse array: 50 -40 -30 20 10
+// Click me to see the solution
+// 94. Write a C# Sharp program to find out the maximum element between the first and last element in a given array of integers ( length 4), replace all elements with the maximum element.
 
-Sample Input:
-{ 10, 20, -30, -40 }
-Expected Output:
+// Sample Input:
+// { 10, 20, -30, -40 }
+// Expected Output:
 
-New array: 20 20 20 20
-Click me to see the solution
-95. Write a C# Sharp program to create an array containing the middle elements from the two given arrays of integers, each length 5.
+// New array: 20 20 20 20
+// Click me to see the solution
+// 95. Write a C# Sharp program to create an array containing the middle elements from the two given arrays of integers, each length 5.
 
-Sample Input:
-{ 10, 20, -30, -40, 30 }, { 10, 20, 30, 40, 30 }
-Expected Output:
+// Sample Input:
+// { 10, 20, -30, -40, 30 }, { 10, 20, 30, 40, 30 }
+// Expected Output:
 
-New array: -30 30
-Click me to see the solution
-96. Write a C# Sharp program to create an array taking the first and last elements of a given array of integers and length 1 or more.
+// New array: -30 30
+// Click me to see the solution
+// 96. Write a C# Sharp program to create an array taking the first and last elements of a given array of integers and length 1 or more.
 
-Sample Input:
-{ 10, 20, -30, -40, 30 }
-Expected Output :
+// Sample Input:
+// { 10, 20, -30, -40, 30 }
+// Expected Output :
 
-New array: 10 30
-Click me to see the solution
-97. Write a C# Sharp program to check if an array of integers length 2 contains 15 or 20.
+// New array: 10 30
+// Click me to see the solution
+// 97. Write a C# Sharp program to check if an array of integers length 2 contains 15 or 20.
 
-Sample Input:
-{ 12, 20 }
-{ 14, 15 }
-{ 11, 21 }
-Expected Output :
+// Sample Input:
+// { 12, 20 }
+// { 14, 15 }
+// { 11, 21 }
+// Expected Output :
 
-True
-True
-False
-Click me to see the solution
-98. Write a C# Sharp program to check if an array of integers with length 2 does not contain 15 or 20.
+// True
+// True
+// False
+// Click me to see the solution
+// 98. Write a C# Sharp program to check if an array of integers with length 2 does not contain 15 or 20.
 
-Sample Input:
-{ 12, 20 }
-{ 14, 15 }
-{ 11, 21 }
-Expected Output :
+// Sample Input:
+// { 12, 20 }
+// { 14, 15 }
+// { 11, 21 }
+// Expected Output :
 
-False
-False
-True
-Click me to see the solution
-99. Write a C# Sharp program to create an array of integers with a length of 1 or more. The array length will be double the given array length. All the elements are 0 except the first element which is equal to the given array.
+// False
+// False
+// True
+// Click me to see the solution
+// 99. Write a C# Sharp program to create an array of integers with a length of 1 or more. The array length will be double the given array length. All the elements are 0 except the first element which is equal to the given array.
 
-Sample Input:
-{ 10, 20, -30, -40, 30 }
-Expected Output :
+// Sample Input:
+// { 10, 20, -30, -40, 30 }
+// Expected Output :
 
-New array: 10 0 0 0 0 0 0 0 0 0
-Click me to see the solution
+// New array: 10 0 0 0 0 0 0 0 0 0
+// Click me to see the solution
