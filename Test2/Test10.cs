9d50dca9cddff60b078aa20e645119f6d8c67afe
Sam <samuelkebbell@gmail.com>
2024-12-22 10:56:55 +0000

    Updates

--- Test2/Test10.cs
+++ Test2/Test10.cs
@@ -37,6 +37,8 @@ else
 
 
 
+
+
 // 83. Write a C# Sharp program to create a new string using 3 copies of the first 2 characters of a given string. If the string length is less than 2 use the whole string.
 // Sample Input:
 // "abc"
